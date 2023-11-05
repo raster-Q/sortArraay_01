288ec9114576bb7c55ee75d862535199bf9b704b
raster-Q <119475152+raster-Q@users.noreply.github.com>
2023-11-06 01:37:54 +0900

    1、初期の足し算判定、オブジェクト

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import "./styles.css";
 
-let arr = [null];
-function sortArray (arr) {
-let n = arr.length;
-let temp, i;
+let sortArray = (arr, t) => {
+  let items = [];
+  let item = {
+    arr: [],
+    formula: `${arr[0]} + ${arr[1]} + ${arr[2]} + ${arr[3]}`,
+    total: arr[0] + arr[1] + arr[2] + arr[3] === t ? "〇" : "×"
+  };
+  item.arr = arr;
+  items.push(item);
+  return items;
+};
 
-}
+let result = sortArray([1, 2, 5, 4], 12);
+console.log(result);
